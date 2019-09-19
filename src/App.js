ecea1c19fcbccabd65af2263ee861869cac348a2
unknown <fody@protonmail.com>
2019-09-19 23:11:43 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import './App.css';
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
 
 const App = (props) => {
   return (
     <div className="App">
-      <Header />
-      <Footer />
+
     </div>
   );
 };
--- src/components/Footer/Footer.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import style from './Footer.module.css';
-
-export default props => (
-    <footer>
-      <p>Hi</p>
-    </footer>
-);
