ac7321c914742adf507307c484eccc0882cf4408
Boris Brovin <45284853+Brovin@users.noreply.github.com>
2019-10-14 19:00:56 +0300

    update

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import {combineReducers, createStore} from 'redux';
+import usersReducer from './users-reducer';
 
 let reducers = combineReducers({
-
+  users: usersReducer
 });
 
 let store = createStore(reducers);
