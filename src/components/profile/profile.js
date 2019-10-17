8acddb33d1ab2d22c599660b42a57a6b070e13fd
Boris Brovin <45284853+Brovin@users.noreply.github.com>
2019-10-18 02:39:12 +0300

    update

--- src/components/profile/profile.js
+++ src/components/profile/profile.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import styles from './profile.module.css';
+import ProfileInfo from "./profileInfo";
 
 const Profile = (props) => {
   return(
     <div className={styles.profile}>
       <h1>Profile</h1>
-      {console.log(props.profile.fullName)}
+      <ProfileInfo profile={props.profile} />
     </div>
   )
 };
--- src/components/profile/profileInfo/profileInfo.js
+++ src/components/profile/profileInfo/profileInfo.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const ProfileInfo = (props) => {
-
+  return(
+    <div>
+      <img src={props.profile.photos.large} alt="" />
+    </div>
+  )
 };
 
 export default ProfileInfo;
