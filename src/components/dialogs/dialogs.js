bfd6a8629f4552071bbd35539d58ed7a9cca1fc7
Boris Brovin <45284853+Brovin@users.noreply.github.com>
2019-10-23 18:44:25 +0300

    add dialogs form

--- src/components/dialogs/dialogs.js
+++ src/components/dialogs/dialogs.js
@@ -1,16 +1,42 @@
 import React from 'react';
 import DialogItem from "./dialog-item";
+import {Field, reduxForm} from "redux-form";
 
 const Dialogs = (props) => {
+
+  let addNewMessage = (values) => {
+    console.log(values.newMessageBody)
+  };
+
   return(
     <div>
-      <h1>Dialogs</h1>
-      {props.messages.map(message=> <DialogItem
-        message={message.message}
-        key={message.id}
-      />)}
+      <div>
+        <h1>Dialogs</h1>
+        {props.messages.map(message=> <DialogItem
+          message={message.message}
+          key={message.id}
+        />)}
+      </div>
+      <AddMessageFormRedux onSubmit={addNewMessage} />
     </div>
   )
 };
+
+const AddMessageForm = (props) => {
+  return(
+    <div>
+      <form onSubmit={props.handleSubmit}>
+        <div>
+          <Field component="textarea" name="newMessageBody" placeholder="Enter your message" />
+        </div>
+        <div>
+          <button>Send</button>
+        </div>
+      </form>
+    </div>
+  )
+};
+
+const AddMessageFormRedux = reduxForm({form: "dialogAddMessageForm"})(AddMessageForm);
 
 export default Dialogs;
