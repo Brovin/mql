9050130ad5e444536db7a7d47d45943514b6125a
Boris Brovin <45284853+Brovin@users.noreply.github.com>
2019-10-30 20:12:11 +0300

    update paginator

--- src/components/users/users.js
+++ src/components/users/users.js
@@ -1,24 +1,18 @@
 import React from 'react';
 import styles from './users.module.css';
 import User from "./user";
+import Paginator from "../common/paginator";
 
 const Users = React.memo(props => {
-  let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
-
-  let pages = [];
-  for (let i = 1; i <= pagesCount; i++) {
-    pages.push(i);
-  }
   return(
     <div className={styles.users}>
       <h1>Users</h1>
-      <div>
-        {pages.map(page => <span
-          className={props.currentPage === page ? styles.selectedPage : null}
-          key={page}
-          onClick={() => props.onPageChanged(page)}
-        >{page}</span>)}
-      </div>
+      <Paginator
+        totalItemsCount={props.totalUsersCount}
+        pageSize={props.pageSize}
+        currentPage={props.currentPage}
+        onPageChanged={props.onPageChanged}
+      />
       {props.users.map(user =>
         <User
           user={user}
